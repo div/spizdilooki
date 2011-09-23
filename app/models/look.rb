ccf58c1590e92a8c877f0facd727e17034e0ece2
Igor Davydov <iskiche@gmail.com>
2011-09-23 14:30:12 +0400

    Setup Carrierwave uploads

--- app/models/look.rb
+++ app/models/look.rb
@@ -1,8 +1,16 @@
 class Look < ActiveRecord::Base
   after_create :fetch_lookatmedata
   scope :published, :conditions => {:processing => false}
+  mount_uploader :image, ImageUploader
   
-  
+  def fetch_image
+    if self.source_img_url
+      logger.info("Processing #{self.source_img_url} for #{self.name}")
+      self.remote_image_url = self.source_img_url # Saving image with carrierwave
+      self.image.store!
+      self.processing = false
+    end
+  end
   
   private 
   
