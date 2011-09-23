1192913f60efac180cde81b1da66475b16e01c5c
Igor Davydov <iskiche@gmail.com>
2011-09-23 18:52:32 +0400

    Using heroku vars for autoscaler

--- config/initializers/resque_heroku_autoscaler_setup.rb
+++ config/initializers/resque_heroku_autoscaler_setup.rb
@@ -1,7 +1,7 @@
 require 'resque/plugins/resque_heroku_autoscaler'
 
 Resque::Plugins::HerokuAutoscaler.config do |c|
-  c.heroku_user = "iskiche@gmail.com"
-  c.heroku_pass = "348797"
-  c.heroku_app  = "spizdilooki"
+  # c.heroku_user = "iskiche@gmail.com"
+  # c.heroku_pass = "348797"
+  # c.heroku_app  = "spizdilooki"
 end
