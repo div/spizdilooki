dca33101b19fa10de70161602f9d49f8e9068396
Igor Davydov <iskiche@gmail.com>
2011-09-23 10:51:05 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spilook::Application.config.secret_token = 'd0ebab595685e337a384a19ccd4a9035f7aa21d9981e61b1c2908eb56135ea9c5ef9591dc3893256342c6900f7ebb8aa7d58786a4179aba073eef4754a7dd771'
