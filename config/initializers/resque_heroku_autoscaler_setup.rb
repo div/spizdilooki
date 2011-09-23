20f3760b94a8749a40cea019a685b6a2b49734e2
Igor Davydov <iskiche@gmail.com>
2011-09-23 18:44:14 +0400

    Change autoscaler settings

--- config/initializers/resque_heroku_autoscaler_setup.rb
+++ config/initializers/resque_heroku_autoscaler_setup.rb
@@ -3,5 +3,5 @@ require 'resque/plugins/resque_heroku_autoscaler'
 Resque::Plugins::HerokuAutoscaler.config do |c|
   c.heroku_user = "iskiche@gmail.com"
   c.heroku_pass = "348797"
-  c.heroku_app  = "spizdilooki_#{Rails.env}"
+  c.heroku_app  = "spizdilooki"
 end
